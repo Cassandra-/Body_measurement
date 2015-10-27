61e0c10d0ef19a9547876def0f7cc07a453b3800
Cassandra Loor <the_cas02@hotmail.com>
2015-10-27 20:09:20 +0100

    added mean shift and recalculation of clusters

--- Python/file2matrix.py
+++ Python/file2matrix.py
@@ -1,8 +1,10 @@
 import numpy as np
-import csv
-from pylab import *
 from mpl_toolkits.mplot3d import axes3d
 import matplotlib.pyplot as plt
+import argparse
+import os
+import sys
+from sklearn.cluster import MeanShift, estimate_bandwidth
 
 #load pointcloud data
 #is expected to be stored as csv:
@@ -10,7 +12,7 @@ import matplotlib.pyplot as plt
 #X2,Y2,Z2,1
 #etc...
 def loadfile(filename):
-    return np.loadtxt(filename, delimiter=',')    
+    return np.loadtxt(filename, delimiter=',')
 
 def get_cloud_center(cloud):
     #get center of the cloud
@@ -31,7 +33,7 @@ def fitPlaneLstsq(cloud):
     A = np.column_stack((cloud[:,0],cloud[:,1],cloud[:,3]))
     b = cloud[:,2]
     return np.linalg.lstsq(A, b)[0]
-    
+
 #Alternate fit plane according to the least squares solution
 #Solve Ax + By + Cz  = -D
 def fitPlaneLstsqAlt(cloud):
@@ -49,11 +51,11 @@ def fitPlaneSVD(cloud):
     V = V.conj().T
     return V[-1]
 
-#project cloudpoints. Only for least squares at the moment    
+#project cloudpoints. Only for least squares at the moment
 def project_cloud2(cloud, plane):
     new_cloud = np.column_stack((cloud[:,0],cloud[:,1],cloud[:,2]))
     projected_cloud = []
-    for row in new_cloud:  
+    for row in new_cloud:
         d = (np.dot(row, plane)) / np.sqrt(np.linalg.norm(plane))
         normalized_plane = plane / np.linalg.norm(plane)
         proj =d*normalized_plane
@@ -64,26 +66,26 @@ def get_d(plane_normal, cloud):
     #Ax + By + Cz = -D
     cloud_center = get_cloud_center(cloud)
     return -plane_normal.dot(cloud_center)
-    
+
 #calulate the direction of the line and return the normalized direction
 def plane_intersection(plane1, plane2, cloud1, cloud2):
     new_cloud1 = np.column_stack((cloud1[:,0],cloud1[:,1],cloud1[:,2]))
     cloud_center1 = get_cloud_center(new_cloud1)
     plane1_d  = np.dot(cloud_center1, plane1)
-    
+
     new_cloud2 = np.column_stack((cloud2[:,0],cloud2[:,1],cloud2[:,2]))
     cloud_center2 = get_cloud_center(new_cloud2)
     plane2_d = np.dot(cloud_center2, plane2)
-    
+
     plane3 = np.cross(plane1, plane2)
 
     A = np.row_stack((plane1, plane2, plane3))
     b = np.asarray([plane1_d, plane2_d, 0])
     point_on_line = np.linalg.solve(A, b)
-    
+
     direction = np.cross(plane1, plane2)
     return direction/np.linalg.norm(direction), point_on_line
-    
+
 def proj_cloud(cloud, plane_normal):
     #get center of plane
     new_cloud = np.column_stack((cloud[:,0],cloud[:,1],cloud[:,2]))
@@ -112,7 +114,7 @@ def project_cloud(cloud, plane):
                        proj_onto_normal_vector[:, None]*normal_vector)
 
     return point_in_plane + proj_onto_plane
-    
+
 #assumes direction vector is unit vector
 def proj_cloud_line(cloud, direction, offset):
     #(x,y,z).(a,b,c) (a,b,c) = (ax+by+cz)(a,b,c)
@@ -122,7 +124,7 @@ def proj_cloud_line(cloud, direction, offset):
         projection = (np.dot(point, direction)*direction) + offset
         proj_cloud.append(projection)
     return np.asarray(proj_cloud)
-    
+
 #find the location of the joint
 #take the center of the cloud
 #assume the point closest to this is the location of the joint
@@ -132,7 +134,7 @@ def find_joint_location(cloud1, cloud2, line1, line2):
     center_cloud1 = get_cloud_center(cloud1_)
     center_cloud2 = get_cloud_center(cloud2_)
     average_c1c2 = (center_cloud1 + center_cloud2) / 2
-    
+
     min_dist = 1000
     min_dist_point = [0,0,0]
     for point in line1:
@@ -140,14 +142,26 @@ def find_joint_location(cloud1, cloud2, line1, line2):
         if dist < min_dist:
             min_dist = dist
             min_dist_point = point
-            
+
     for point in line2:
         dist = np.linalg.norm(point-average_c1c2)
         if dist < min_dist:
             min_dist = dist
             min_dist_point = point
-    
+
     return min_dist_point
+
+def perform_meanshift(cloud1, cloud2):
+    # The following bandwidth can be automatically detected using
+    full_cloud = np.vstack((cloud1, cloud2))
+    bandwidth = estimate_bandwidth(full_cloud, quantile=0.2, n_samples=500)
+
+    # Create MeanShift instance
+    ms = MeanShift(bandwidth=bandwidth, bin_seeding=True)
+    # Perform mean shift
+    ms.fit(full_cloud)
+
+    return full_cloud, ms.cluster_centers_
 
 #plot everything
 def plot_results(cloud1, cloud2, proj_plane1, proj_plane2, arm_line_proj, forearm_line_proj, joint_loc):
@@ -156,7 +170,7 @@ def plot_results(cloud1, cloud2, proj_plane1, proj_plane2, arm_line_proj, forear
     # plot points in 3D
     ax.plot(cloud1[:,0],cloud1[:,1],cloud1[:,2],'o', color='blue')
     ax.plot(cloud2[:,0],cloud2[:,1],cloud2[:,2],'o', color='green')
-    
+
     fig2 = plt.figure()
     ax2 = fig2.gca(projection='3d')
     ax2.plot(cloud1[:,0],cloud1[:,1],cloud1[:,2],'o', color='blue')
@@ -166,38 +180,223 @@ def plot_results(cloud1, cloud2, proj_plane1, proj_plane2, arm_line_proj, forear
 
     fig5 = plt.figure()
     ax5 = fig5.gca(projection='3d')
-    
+
     ax5.plot(cloud1[:,0],cloud1[:,1],cloud1[:,2],'o', color='blue')
     ax5.plot(cloud2[:,0],cloud2[:,1],cloud2[:,2],'o', color='green')
     ax5.plot(arm_line_proj[:,0], arm_line_proj[:,1], arm_line_proj[:,2], 'o', color='red')
     ax5.plot(forearm_line_proj[:,0], forearm_line_proj[:,1], forearm_line_proj[:,2], 'o', color='magenta')
     ax5.plot([joint_loc[0]], [joint_loc[1]], [joint_loc[2]], 'o', color='yellow')
     plt.show()
-    
+
 #draw a line between two points
 #accepts tuples
-def drawline(point1, point2):
-    pyplot.plot([point1[0], point2[0]], [point1[1], point2[1]])
+def drawlimb(point1, point2, ax2=None):
+    ax2.plot([point1[0], point2[0]], [point1[1], point2[1]], [point1[2], point2[2]])
+
+def drawlimb_2d(point1, point2, ax2=None):
+    ax2.plot([point1[0], point2[0]], [point1[1], point2[1]])
+
+def drawskeleton_2d(filename):
+    fig3 = plt.figure()
+    ax3 = fig3.gca()
+
+    lines = [line.rstrip('\n').split(',') for line in open(filename)]
+    joints = []
+    for line in lines:
+        if len(line) == 3:
+            line = map(float, line)
+            line = project3dto2d(line)
+            ax3.plot([line[0]],[line[1]],'o')
+            joints.append(line)
+
+    #draw limbs
+    drawlimb_2d(joints[10], joints[20], ax2=ax3)
+    drawlimb_2d(joints[10], joints[19], ax2=ax3)
+    drawlimb_2d(joints[10], joints[26], ax2=ax3)
+    drawlimb_2d(joints[10], joints[25], ax2=ax3)
+    drawlimb_2d(joints[26], joints[23], ax2=ax3)
+    drawlimb_2d(joints[25], joints[24], ax2=ax3)
+    drawlimb_2d(joints[21], joints[22], ax2=ax3)
+
+    drawlimb_2d(joints[23], joints[8], ax2=ax3)
+    drawlimb_2d(joints[24], joints[9], ax2=ax3)
+    drawlimb_2d(joints[9], joints[2], ax2=ax3)
+    drawlimb_2d(joints[8], joints[6], ax2=ax3)
+    drawlimb_2d(joints[2], joints[0], ax2=ax3)
+    drawlimb_2d(joints[6], joints[4], ax2=ax3)
+
+    drawlimb_2d(joints[26], joints[12], ax2=ax3)
+    drawlimb_2d(joints[12], joints[14], ax2=ax3)
+    drawlimb_2d(joints[25], joints[16], ax2=ax3)
+    drawlimb_2d(joints[16], joints[18], ax2=ax3)
+    drawlimb_2d(joints[19], joints[21], ax2=ax3)
+    drawlimb_2d(joints[20], joints[22], ax2=ax3)
+
+    drawlimb_2d(joints[26], joints[9], ax2=ax3)
+    drawlimb_2d(joints[25], joints[8], ax2=ax3)
+    drawlimb_2d(joints[8], joints[9], ax2=ax3)
+    plt.show()
 
 #draw skeleton
-def drawskeleton(skeleton):
+def drawskeleton(filename):
+    fig2 = plt.figure()
+    ax2 = fig2.gca(projection='3d')
+
+    lines = [line.rstrip('\n').split(',') for line in open(filename)]
+    joints = []
+    for line in lines:
+        if len(line) == 3:
+            line = map(float, line)
+            ax2.plot([line[0]],[line[1]],[line[2]],'o')
+            joints.append(line)
+
+    #draw limbs
+    drawlimb(joints[10], joints[20], ax2=ax2)
+    drawlimb(joints[10], joints[19], ax2=ax2)
+    drawlimb(joints[10], joints[26], ax2=ax2)
+    drawlimb(joints[10], joints[25], ax2=ax2)
+    drawlimb(joints[26], joints[23], ax2=ax2)
+    drawlimb(joints[25], joints[24], ax2=ax2)
+    drawlimb(joints[21], joints[22], ax2=ax2)
+
+    drawlimb(joints[23], joints[8], ax2=ax2)
+    drawlimb(joints[24], joints[9], ax2=ax2)
+    drawlimb(joints[9], joints[2], ax2=ax2)
+    drawlimb(joints[8], joints[6], ax2=ax2)
+    drawlimb(joints[2], joints[0], ax2=ax2)
+    drawlimb(joints[6], joints[4], ax2=ax2)
+
+    drawlimb(joints[26], joints[12], ax2=ax2)
+    drawlimb(joints[12], joints[14], ax2=ax2)
+    drawlimb(joints[25], joints[16], ax2=ax2)
+    drawlimb(joints[16], joints[18], ax2=ax2)
+    drawlimb(joints[19], joints[21], ax2=ax2)
+    drawlimb(joints[20], joints[22], ax2=ax2)
+
+    drawlimb(joints[26], joints[9], ax2=ax2)
+    drawlimb(joints[25], joints[8], ax2=ax2)
+    drawlimb(joints[8], joints[9], ax2=ax2)
+    plt.show()
+
+
+def project_cloud3dto2d(cloud):
+    cloud_2d = []
+    for point in cloud:
+        point = np.asarray(point)
+        point = np.delete(point, 3)
+        cloud_2d.append(project3dto2d(point))
+    return cloud_2d
+
+
+#project 3d point to 2d
+def project3dto2d(point):
+    #Kinect intrinsics
+    kinect_intrinsics = np.asarray([[525.0, 0.0, 319.5], [0.0, 525, 239.5], [0.0, 0.0, 1.0]])
+    m = np.asarray([[1,0,0],[0,1,0],[0,0,1]])
+    proj_matrix = np.dot(kinect_intrinsics, m)
+#    point_3d = point_3d + Eigen::Vector4f (0, 0, 0, 1);  //??
+    projected_point = np.dot(proj_matrix, point)
+    projected_point = projected_point / projected_point [2]
+    projected_point = np.delete(projected_point, 2)
+    return projected_point;
+
+
+def loadfiles(filenum, new_path=None):
+    if new_path is not None:
+        path = new_path
+    fig=plt.figure()
+    fig2d=plt.figure()
+
+    for i in os.listdir(path):
+        fullpath = os.path.join(path, i)
+        if os.path.isfile(fullpath) and i.startswith(str(str(filenum) + '_')):
+            if 'skeleton' in fullpath:
+                drawskeleton(fullpath)
+                drawskeleton_2d(fullpath)
+            else:
+                data = np.loadtxt(fullpath, delimiter=',')
+                if len(data) > 0:
+                    fig = add_plot(data, fig=fig)
+                    fig2d = add_plot_2d(data, fig=fig2d)
+
+
+def add_plot(data, fig=None):
+    ax = fig.gca(projection='3d')
+    # plot points in 3D
+    ax.plot(data[:, 0], data[:, 1], data[:, 2], '.')
+    plt.show()
+    return fig
+
+
+def add_plot_2d(data, fig=None):
+    data = np.asarray(project_cloud3dto2d(data))
+    ax4 = fig.gca()
+    # plot points in 3D
+    ax4.plot(data[:, 0], data[:, 1], '.')
+    plt.show()
+    return fig
+
+
+def load_cloud(nr, cl1, cl2, path="C:/Users/Cassandra/Documents/GitHub/Body_measurement/Python/Data/Test 4 - Normal"):
+    cloud1 = np.loadtxt(str(path + '/' + str(nr) + '_' + cl1 + '.txt'), delimiter=',')
+    cloud2 = np.loadtxt(str(path + '/' + str(nr) + '_' + cl2 + '.txt'), delimiter=',')
+    A = np.column_stack((cloud1[:,0],cloud1[:,1],cloud1[:,2]))
+    B = np.column_stack((cloud2[:,0],cloud2[:,1],cloud2[:,2]))
+    #find cluster centers
+    A_center = get_cloud_center(A)
+    B_center = get_cloud_center(B)
+    print "Original cloud center A: ", A_center
+    print "Original cloud center B: ", B_center
+    full_cloud, new_cluster_centers = perform_meanshift(A, B)
     
+    return assign_to_cluster(full_cloud, new_cluster_centers)
+    
+def assign_to_cluster(cloud, cluster_centers):
+    nr_clouds = len(cluster_centers)
+    new_clouds = {}
+    for i in range(0, nr_clouds):
+        new_clouds[i] = []
+        
+    for point in cloud:
+        shortest_dist = 1000
+        shortest_dist_loc = -1
+        for c in range(0, nr_clouds):
+            dist = np.linalg.norm(point-cluster_centers[c])
+            if dist < shortest_dist:
+                shortest_dist_loc = c
+                shortest_dist = dist
+        new_clouds[shortest_dist_loc].append(point)
+        
+    return new_clouds
 
 if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-filenr', metavar='How many simulations should be run?', type=int)
+    parser.parse_args()
+
+    #load file
+    #loadfiles(5, new_path="/media/cassandra/Local Disk/Documents and Settings/Cassandra/Documents/GitHub/Body_measurement/Python/Data/Test 4 - Normal")
+    #loadfiles(5, new_path="C:\Users\Cassandra\Documents\GitHub\Body_measurement\Python\Data\Test 4 - Normal")
+    
+    load_cloud(5, "Leftarm", "LeftForearm")
+
+'''
     print "EXECUTING SCRIPT!"
+
     #pass data to script
     arm_data = np.loadtxt("24_Larm.txt", delimiter=',')
     forearm_data = np.loadtxt("24_Lforearm.txt", delimiter=',')
-    
+
     arm_plane_normal = fitPlaneLstsqAlt(arm_data)
     forearm_plane_normal = fitPlaneLstsqAlt(forearm_data)
-    
+
     intersection_direction, offset = plane_intersection(arm_plane_normal, forearm_plane_normal, arm_data, forearm_data)
     proj_arm_line = proj_cloud_line(arm_data, intersection_direction, offset)
     proj_forearm_line = proj_cloud_line(forearm_data, intersection_direction, offset)
-    
+
     proj_arm = proj_cloud(arm_data, arm_plane_normal)
     proj_forearm = proj_cloud(forearm_data, forearm_plane_normal)
-    
+
     joint_loc = find_joint_location(arm_data, forearm_data, proj_arm_line, proj_forearm_line)
     #plot_results(arm_data, forearm_data, proj_arm, proj_forearm, proj_arm_line, proj_forearm_line, joint_loc)
+'''
